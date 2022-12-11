c5d9cb6cc6e00b0e174eace4bd2e28743e36a905
alehey <alexey.p.svyz@mail.ru>
2022-12-11 19:11:40 +0700

    add task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Input number");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99)
+{
+    while (a / 10 > 100)
+    {
+        int result = a / 10;
+        a = a / 10;
+    }
+    {
+        Console.WriteLine($"The third digit: {a % 10}");   // int result = a % 10; 
+                                                           // Console.WriteLine("The third digit " + result);
+    }
+}
+else
+    Console.WriteLine("There is no third digit");
