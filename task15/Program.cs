df572b0824765e5fd2d62a290e2bccc05c9241b2
alehey <alexey.p.svyz@mail.ru>
2022-12-11 19:36:19 +0700

    add task15

---
+++ task15/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 7};
+
+Console.WriteLine("Input number 1-7");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 || day > 7)
+    Console.WriteLine("Вы ввели неверное число");
+else if (day > array[4])
+    Console.WriteLine("Да, это выходной");
+else
+    Console.WriteLine("Нет, это рабочий день");
+
+
