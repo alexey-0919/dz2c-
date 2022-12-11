c3b1985faabf704cd6952cc26ad01855402f5af1
alehey <alexey.p.svyz@mail.ru>
2022-12-11 18:34:15 +0700

    add task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input three-digit number");
+int A = Convert.ToInt32(Console.ReadLine());
+
+if (A >99 && A < 1000)
+{
+    int result1 = A / 10;
+    int result2 = result1 % 10;
+    Console.WriteLine ("Second number = " + result2);
+}
+else
+    Console.WriteLine("Введите трехзначное число");
